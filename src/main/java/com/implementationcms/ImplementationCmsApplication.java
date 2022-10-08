7074bb8ba210dd579c9888904df6512dc3125839
marksalch <marksirius96@gmai.com>
2022-10-08 20:30:12 +0800

    Initial commit

---
+++ src/main/java/com/implementationcms/ImplementationCmsApplication.java
@@ -0,0 +1,13 @@
+package com.implementationcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementationCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementationCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/implementationcms/ImplementationCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.implementationcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementationCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
